f8ac8200f98e79d7e35001c8c510c34fd06479fe
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-04-19 18:39:30 +0500

    Variables revision

---
+++ Phase-01_Foundations/03_Variables_DataTypes/variables.js
@@ -0,0 +1,8 @@
+fullName = "Abdul Basit";
+age = 21;
+gender = "Male";
+isFollowed = false;
+height = 5.8;
+girlFriend = null;
+lifeBeauty = undefined;
+console.log("A Comsats Student, Named "+fullName+" "+age+" "+gender+" "+isFollowed+" "+height+" "+girlFriend+" "+lifeBeauty+" ")
