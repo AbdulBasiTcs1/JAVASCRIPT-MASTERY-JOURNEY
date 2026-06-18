e19ba6ccbf4a0895087ebbf17f610ea149aaeff2
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-06-18 12:14:07 +0500

    rectangle area and pass-fail code

---
+++ Problem_Solving/4_rectangle-area.js
@@ -0,0 +1,9 @@
+// area = length * breadth
+
+
+let length = 20
+let breadth = 40
+
+let area = length * breadth
+
+console.log("Area of the rectangle : ",area)
---
+++ Problem_Solving/5_pass-fail.js
@@ -0,0 +1,11 @@
+// 40+ --> pass
+// other wise he is fail
+
+let marks = 39
+
+if (marks>40){
+    console.log("Pass")
+}else{
+    console.log("Fail")
+
+}
