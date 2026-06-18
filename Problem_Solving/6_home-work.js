78c5e178c248fcfa0fb6ffea343b8b7c7e67b154
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-06-18 12:16:53 +0500

    home work

---
+++ Problem_Solving/6_home-work.js
@@ -0,0 +1,6 @@
+// Practice Questions (Solve Yourself)
+// Find the product of two numbers. -> 
+// Find the smaller of two numbers.
+// Check if a number is positive or negative.
+// Calculate the perimeter of a rectangle.
+// Check if age is 18 or above.
